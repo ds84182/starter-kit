b6df25a8364b4ac9e2a691032914c7255008a810
Dwayne Slater <ds84182@gmail.com>
2014-08-08 21:55:46 -0400

    Ok, fixed a crash

--- web/controller/definition.js
+++ web/controller/definition.js
@@ -7,7 +7,7 @@ App.controller('definition', function (page, def) {
 		this.restorable = false;
 	}
 	$(page).find(".app-title").html("Definition: "+definition.term);
-	var deftext = "<b>"+definition.term+"</b><br><p>"+definition.definition+"</p><br><i>Example: </i><p>"+definition.example+"</p><br><a href=\""+definition.url+"\">Link to UrbanDictionary.com</a>";
+	var deftext = "<b>"+definition.term+"</b><br><p>"+definition.definition+"</p><br><i>Example: </i><p>"+definition.example+"</p><br><div class=\"app-button\" onclick=\"kik.open('"+definition.url+"', true);\">Link to UrbanDictionary.com</div>";
 	$(page).find("#definition-section").html(deftext);
 	$(page).find("#share-button").on("click", function () {
 		kik.send({
